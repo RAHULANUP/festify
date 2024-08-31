b95084ca5fe3ac43fa28860f8a4fca6f65cb0f60
Rahul Anup Varma <98415824+RAHULANUP@users.noreply.github.com>
2024-08-31 13:05:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYDyHs1MlYDRKF3HX2U0DVHuSbu8t2Mhs",
-  authDomain: "festify-8912f.firebaseapp.com",
-  projectId: "festify-8912f",
-  storageBucket: "festify-8912f.appspot.com",
-  messagingSenderId: "19466756330",
-  appId: "1:19466756330:web:66ad43766ea85af48c7793",
-  measurementId: "G-9PM7XD6CRW"
+  
 };
 
 
